19590960ed9047b7795f49ff2874d7137449fa45
anushka-chace <anushka.priya@lockrfinance.com>
2023-01-27 09:49:45 +0530

    configured tailwind and made navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import "./components/Navbar"
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+       <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+    return(
+        <>
+        <div className="w-full h-[80px] text-[white] bg-[black]">
+            <div className="w-[200px] flex justify-between align-left absolute right-10 mt-6">
+            <p className="font-semibold">Products</p>
+            <p className="font-semibold">Wishlist</p>
+            </div>   
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
